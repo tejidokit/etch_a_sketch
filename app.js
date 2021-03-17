7991343d1cf3cf0bae8d3bcac6b351116ef05df1
Kit Tejido <kittejido@gmail.com>
2021-03-16 22:48:58 -0600

    created main variables at app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// declaring the variables
+
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("rows")
+let cells = document.getElementsByClassName("cells")
+
+// function creates 16 x 16 grid
