021d7457300b8ac95f648a1b9f8ca4e606d41f2e
Kit Tejido <kittejido@gmail.com>
2021-03-18 04:01:45 -0600

    testing function that creates grid

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
 // declaring the variables
 
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("rows")
-let cells = document.getElementsByClassName("cells")
+// const container = document.getElementById("container");
+// let rows = document.getElementsByClassName("rows")
+// let cells = document.getElementsByClassName("cells")
 
 // function creates 16 x 16 grid
+
+
+// ###################3 
+
+//Sets important constants and variables
+
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+
+defaultGrid();
+//Creates a default grid sized 16x16 
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+//Takes (rows, columns) input and makes a grid
+function makeRows(rowNum) {
+
+    //Creates rows
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+};
+
+//Creates columns
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+
+    };
+};
